e5ae8619a8f40912b04d194ac4f7d0b30c436599
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2023-04-12 14:32:08 +0100

    start login UI

--- lib/general_screens/login_page.dart
+++ lib/general_screens/login_page.dart
@@ -12,8 +12,74 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: getAppColor(true),
-      body: Column(),
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: Column(
+          children: [
+            //Greeting
+            SizedBox(
+              height: 25,
+            ),
+            SafeArea(
+                child: Text(
+              "Hello!",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 42),
+            )),
+
+            Text(
+              "Welcome to TMS",
+              style: TextStyle(fontSize: 32),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            //email textfield
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(18)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: "Email"),
+                  ),
+                ),
+              ),
+            ),
+            //password textfield
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[100],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(18)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Password",
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            //sign in button
+          ],
+        ),
+      ),
     );
   }
 }
