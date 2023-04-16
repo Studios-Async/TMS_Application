960e3214fb24e64e867e8e31daa6a08e5302fc0d
Aryaman Dubey <aryaman.dubey@kcl.ac.uk>
2023-04-16 15:46:12 +0100

    stop overflowing error

--- lib/general_screens/login_page.dart
+++ lib/general_screens/login_page.dart
@@ -15,94 +15,96 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Colors.grey[200],
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //Top icon
-              Container(
-                height: 175,
-                //child: Image.asset("assets/icons/white-board.png"),
-                child: Image.asset("assets/icons/mortarboard.png"),
-              ),
-
-              SizedBox(
-                height: 10,
-              ),
-              //Greeting
-              Text(
-                "Hello!",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 52),
-              ),
-              Text(
-                "Welcome to TMS",
-                style: TextStyle(fontSize: 32),
-              ),
-              SizedBox(
-                height: 55,
-              ),
-              //email textfield
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[100],
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(18)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none, hintText: "Email"),
-                    ),
-                  ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Top icon
+                Container(
+                  height: 175,
+                  //child: Image.asset("assets/icons/white-board.png"),
+                  child: Image.asset("assets/icons/mortarboard.png"),
                 ),
-              ),
-              //password textfield
-              SizedBox(
-                height: 15,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey[100],
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(18)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Password",
+
+                SizedBox(
+                  height: 10,
+                ),
+                //Greeting
+                Text(
+                  "Hello!",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 52),
+                ),
+                Text(
+                  "Welcome to TMS",
+                  style: TextStyle(fontSize: 32),
+                ),
+                SizedBox(
+                  height: 55,
+                ),
+                //email textfield
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: "Email"),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              //sign in button
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius: BorderRadius.circular(18)),
-                  child: Center(
-                      child: Text(
-                    "Sign In",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  )),
+                //password textfield
+                SizedBox(
+                  height: 15,
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Password",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                //sign in button
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Center(
+                        child: Text(
+                      "Sign In",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    )),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
