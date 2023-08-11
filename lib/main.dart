746a582a22951f68c7f926258295aaf5bfd3e770
Arham <ArhmKhn007@gmail.com>
2023-08-11 16:38:57 +0100

    needed import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tms/Utilities/navbar.dart';
 import 'package:tms/general_screens/login_page.dart';
 import 'package:tms/general_screens/welldone_page.dart';
 
