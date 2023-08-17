cdb5744b59344889a915078a2314ea8954afa68d
Arham <ArhmKhn007@gmail.com>
2023-08-17 13:34:01 +0100

    Revert "starting smth to store lessons"
    This reverts commit 324321de1936c035231640d31899c55d733e8963.

--- lib/Utilities/lessons_created.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Lesson {
-  final String title;
-  Lesson(this.title);
-}
--- lib/Utilities/student_calendar.dart
+++ lib/Utilities/student_calendar.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:tms/Utilities/lessons_created.dart';
-import 'package:tms/Utilities/lessons_created.dart';
-
-import 'create_new_lesson.dart';
-import 'lessons_created.dart';
 
 class StudentCalendar extends StatefulWidget {
   const StudentCalendar({Key? key}) : super(key: key);
@@ -17,46 +12,40 @@ class _StudentCalendarState extends State<StudentCalendar> {
   final CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime _selectedDay = DateTime.now();
-  Map<DateTime, List<Lesson>> lesson = {}; //Stores created lessons
   TextStyle weekstyle =
       const TextStyle(fontSize: 20, fontWeight: FontWeight.w500);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TableCalendar(
-          rowHeight: 55,
-          headerStyle: const HeaderStyle(
-            formatButtonVisible: false,
-            titleCentered: true,
-            titleTextStyle: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-            ),
-            leftChevronMargin: EdgeInsets.only(left: 60),
-            rightChevronMargin: EdgeInsets.only(right: 60),
-          ),
-          daysOfWeekHeight: 50,
-          daysOfWeekStyle:
-              DaysOfWeekStyle(weekdayStyle: weekstyle, weekendStyle: weekstyle),
-          calendarFormat: _calendarFormat,
-          focusedDay: _focusedDay,
-          firstDay: DateTime.utc(2000, 1, 1),
-          lastDay: DateTime.utc(2030, 12, 31),
-          startingDayOfWeek: StartingDayOfWeek.monday,
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
-            });
-          },
+    return TableCalendar(
+      rowHeight: 55,
+      headerStyle: const HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+        titleTextStyle: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
         ),
-        NewLesson(selectedDay: _selectedDay),
-      ],
+        leftChevronMargin: EdgeInsets.only(left: 60),
+        rightChevronMargin: EdgeInsets.only(right: 60),
+      ),
+      daysOfWeekHeight: 50,
+      daysOfWeekStyle:
+          DaysOfWeekStyle(weekdayStyle: weekstyle, weekendStyle: weekstyle),
+      calendarFormat: _calendarFormat,
+      focusedDay: _focusedDay,
+      firstDay: DateTime.utc(2000, 1, 1),
+      lastDay: DateTime.utc(2030, 12, 31),
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      selectedDayPredicate: (day) {
+        return isSameDay(_selectedDay, day);
+      },
+      onDaySelected: (selectedDay, focusedDay) {
+        setState(() {
+          _selectedDay = selectedDay;
+          _focusedDay = focusedDay;
+        });
+      },
     );
   }
 }
