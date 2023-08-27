374c964633f4eb980cc041c9de94d70d1ec003e4
Arham <ArhmKhn007@gmail.com>
2023-08-27 19:18:29 +0100

    hyaans profile page fixed by me

--- lib/general_screens/Tutor/Utilities/tutor_navbar.dart
+++ lib/general_screens/Tutor/Utilities/tutor_navbar.dart
@@ -4,7 +4,7 @@ import 'package:tms/general_screens/Student/this_months_learning.dart';
 import 'package:tms/general_screens/lesson_request.dart';
 import 'package:tms/general_screens/Tutor/tutor_home.dart';
 import 'package:tms/general_screens/Student/welldone_page.dart';
-import 'package:tms/general_screens/profile_page.dart';
+import 'package:tms/general_screens/Student/student_profile_page.dart';
 
 class TutorNavBar extends StatefulWidget {
   const TutorNavBar({Key? key}) : super(key: key);
@@ -19,7 +19,7 @@ class _TutorNavBarState extends State<TutorNavBar> {
   List<Widget> pages = [
     const TutorHome(),
     const LessonPage(),
-    ProfilePage()
+    StudentProfilePage()
     //ThisMonthsLearningPage() //temporary
   ]; // List of pages
 
