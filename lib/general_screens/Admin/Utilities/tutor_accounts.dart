2e89b172152bc2c4793f2900559a5d3b983a35be
Arham <ArhmKhn007@gmail.com>
2023-09-02 18:49:39 +0100

    start

---
+++ lib/general_screens/Admin/Utilities/tutor_accounts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TutorAccountsList extends StatefulWidget {
+  const TutorAccountsList({super.key});
+
+  @override
+  State<TutorAccountsList> createState() => _TutorAccountsListState();
+}
+
+class _TutorAccountsListState extends State<TutorAccountsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/general_screens/Admin/admin_accounts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminAccounts extends StatefulWidget {
+  const AdminAccounts({super.key});
+
+  @override
+  State<AdminAccounts> createState() => _AdminAccountsState();
+}
+
+class _AdminAccountsState extends State<AdminAccounts> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
