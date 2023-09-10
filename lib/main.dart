8de4888d52821cd753ec774ef84dc6e28c764db3
Arham <ArhmKhn007@gmail.com>
2023-09-10 18:18:35 +0100

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tms/general_screens/Admin/Utilities/admin_navbar.dart';
+import 'package:tms/general_screens/Student/Utilities/student_navbar.dart';
 import 'package:tms/general_screens/Tutor/tutor_profile.dart';
+import 'package:tms/general_screens/profile%20page/profile_page.dart';
 import 'general_screens/Admin/admin_profile.dart';
 
 Future main() async {
@@ -16,12 +18,11 @@ Future main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false, //dont show banner in corner
-      home: TutorProfilePage(),
+      home: StudentNavBar(),
     );
   }
 }
