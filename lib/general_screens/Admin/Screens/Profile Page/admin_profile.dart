8a6c143ebaf2fdb2d54ad7d55ba662e29b8405a7
Arham <ArhmKhn007@gmail.com>
2023-10-06 17:50:23 +0100

    tiny little change

--- lib/general_screens/Admin/Screens/Profile Page/admin_profile.dart
+++ lib/general_screens/Admin/Screens/Profile Page/admin_profile.dart
@@ -37,56 +37,80 @@ class _AdminProfilePageState extends State<AdminProfilePage> {
                   ),
                 ),
               ),
-              const Text(
-                'Admin Name',
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 24),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8.0, vertical: 12),
-                      child: NewButton(
-                        buttonheight: screenWidth * 0.25,
-                        buttonwidth: screenWidth * 0.95,
-                        circle: false,
-                        usingIcon: false,
-                        text: "Change Email",
-                        textsize: logicalWidth * 0.06,
-                        onPressed: () {},
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8.0, vertical: 12),
-                      child: NewButton(
-                        buttonheight: screenWidth * 0.25,
-                        buttonwidth: screenWidth * 0.95,
-                        circle: false,
-                        usingIcon: false,
-                        text: "Change Password",
-                        textsize: logicalWidth * 0.06,
-                        onPressed: () {},
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8.0, vertical: 12),
-                      child: NewButton(
-                        circle: false,
-                        buttonheight: screenWidth * 0.25,
-                        buttonwidth: screenWidth * 0.95,
-                        usingIcon: false,
-                        text: "Log Out",
-                        textsize: logicalWidth * 0.06,
-                        onPressed: () {},
-                      ),
-                    ),
-                  ],
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Admin Name',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
                 ),
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.01,
+                        vertical: screenWidth * 0.05),
+                    child: NewButton(
+                      circle: false,
+                      buttonheight: screenWidth * 0.25,
+                      buttonwidth: screenWidth * 0.95,
+                      usingIcon: false,
+                      text: "Change Email",
+                      //SubjectsPage
+                      textsize: screenWidth * 0.06,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Placeholder(),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.01,
+                        vertical: screenWidth * 0.05),
+                    child: NewButton(
+                      buttonheight: screenWidth * 0.25,
+                      buttonwidth: screenWidth * 0.95,
+                      usingIcon: false,
+                      circle: false,
+                      text: "Change Password",
+                      textsize: screenWidth * 0.06,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Placeholder(),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenWidth * 0.01,
+                        vertical: screenWidth * 0.05),
+                    child: NewButton(
+                      circle: false,
+                      buttonheight: screenWidth * 0.25,
+                      buttonwidth: screenWidth * 0.95,
+                      usingIcon: false,
+                      text: "Log Out",
+                      textsize: screenWidth * 0.06,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Placeholder(),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
