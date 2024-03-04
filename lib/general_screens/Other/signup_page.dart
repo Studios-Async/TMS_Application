3dddfb34638e3df38fb8ce5d2f94fc4726f2a51d
hyaanr <hyaanr@gmail.com>
2024-03-04 19:14:57 +0000

    1

--- lib/general_screens/Other/signup_page.dart
+++ lib/general_screens/Other/signup_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:tms/Utilities/buttontemplate.dart';
-import 'package:tms/Utilities/data.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -14,30 +12,18 @@ class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  bool _isLoading = false;
 
   void _signUp() async {
-    setState(() {
-      _isLoading = true;
-    });
     try {
-      final UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-        email: _emailController.text.trim(),
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: _emailController.text,
         password: _passwordController.text,
       );
-      // If sign up successful, you can navigate to the next screen or perform any other action.
-      print('Sign up successful: ${userCredential.user!.email}');
-      // Example: Navigate to home page
-      Navigator.pushReplacementNamed(context, '/home');
+      // User created successfully, you can navigate to the next screen or perform other actions.
+      print('User created: ${userCredential.user!.email}');
     } catch (e) {
-      // If sign up fails, display an error message.
-      print('Sign up failed: $e');
-      // You can show an error message using a SnackBar or AlertDialog
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sign up failed: $e')));
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
+      // Error occurred during sign-up, handle it appropriately.
+      print('Failed to create user: $e');
     }
   }
 
@@ -67,17 +53,10 @@ class _SignUpPageState extends State<SignUpPage> {
               obscureText: true,
             ),
             SizedBox(height: 24.0),
-            _isLoading
-                ? CircularProgressIndicator()
-                : NewButton(
-                    buttonheight: 50,
-                    buttonwidth: double.infinity,
-                    usingIcon: false,
-                    text: 'Sign Up',
-                    textsize: 20,
-                    circle: false,
-                    onPressed: _signUp,
-                  ),
+            ElevatedButton(
+              onPressed: _signUp,
+              child: Text('Sign Up'),
+            ),
           ],
         ),
       ),
